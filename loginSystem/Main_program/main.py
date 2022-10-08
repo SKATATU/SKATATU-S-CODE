16532b3ec4a5ceef8dce4cdfcd498fbdc2af38ab
SKATATU <s1810469@gmail.student.hpccss.edu.hk>
2022-10-08 17:17:27 +0800

    dquote> to do: learn the pathlib module for the file path opening dquote> and finish the secltion() in Loginsystem

--- loginSystem/Main_program/main.py
+++ loginSystem/Main_program/main.py
@@ -1,16 +1,58 @@
 from encodings import utf_8
+import select
+from pathlib import Path
+from tkinter.tix import Select
 import bcrypt
 import time
 
 from PP import Password_dict, Username_dict
 
-
 UserName = ""
 PassWord = ""
 LoginTimes = 0
 Process = 0
-
 SignupTimes = 0
+
+script_dir = Path("/Users/edward/first_git/loginSystem/Main_program/bin_123.txt").parent
+bubble_file = script_dir / 'bubble_sort.txt'
+
+
+def selection():
+  print("\n1.bubble_sort.py", "\nenter END to end the process.")
+  print("\n2.login_greating.py", "\nenter END to end the process.")
+  print("\n3.number_accending.py", "\nenter END to end the process.")
+  print("\nenter CANCEL to cancel the selection process.")
+
+  Select = input("\nPlease enter the NUMBER\
+  (without .\
+  ) from above that the program you would like to see the code.")
+
+  if Select == "1":
+    print("")
+    file1 = open("bubble_sort.txt")
+    file1.read()
+    print("")
+    print("Selection():Finished process! Goodbye!")
+    exit()
+
+  if Select == "2":
+    print("")
+    file2 = open(r'login_greeting.txt','r')
+    file2.read()
+    print("")
+    print("Selection():Finished process! Goodbye!")
+    exit()
+  if Select == "3":
+    print("")
+    file3 = open(r'bubble_sort.txt','r')
+    file3.read()
+    print("")
+    print("Selection():Finished process! Goodbye!")
+    exit()
+  if Select == "CANCEL":
+    exit(selection)
+
+
 
 while LoginTimes <= 5:
   if Process == 0: 
@@ -18,15 +60,8 @@ while LoginTimes <= 5:
     print("Enter SIGNUP if you want to sign up an account!")
     UserName = input("\nEnter Username: ") 
 
-  if UserName == "CANCEL" or PassWord == "CANCEL":
-    print("\nThe process has been cancelled")
-    time.sleep(2)
-    LoginTimes += 1
-    Process = 0
-    UserName = ""
-    PassWord = ""
 
-  if UserName == "SIGNUP" or PassWord == "SIGNUP":
+  if UserName == "SIGNUP" or PassWord == "SIGNUP": #the sign up process
     UserName = input("\nPlease enter your Username:")
     Username_dict.update({"username_set" : UserName })
 
@@ -51,12 +86,14 @@ Process >= 1:
       time.sleep(1)  
       print ("\nLogin successful!")
       time.sleep(1)
-      print ("\nWelcome to the -----")
+      print ("\nWelcome to the selection process")
       time.sleep(2)    
       Process = 0
       print("")
+      selection() #run the selection proccess
       print("out come 1")
-      import selection_
+      
+    else:
       print("out come 2")
       exit()
 
