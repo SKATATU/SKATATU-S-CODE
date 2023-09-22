6a1fcb4f9bfcfafdf770a98712a725549adcbced
SKATATU <s1810469@student.hpccss.edu.hk>
2023-09-22 19:36:28 +0800

    upload puzzle_game with replay function

--- books programs/puzzle_game.py
+++ books programs/puzzle_game.py
@@ -1,7 +1,5 @@
 import random
-rows, cols = (3,3)
 puzzle = [[1,2,3],[4,5,6],[7,8,0]]
-
 spaceR=2
 spaceC=2
 move = 0
@@ -14,33 +12,6 @@ def printPuzz():
      
         print (puzzle[i][j],end='')
       print(" ")
-
-#discardable
-"""
-def detect():
-  global detecting
-  print("debug detect")
-  
-#detect if the side is wall
-#count loop for detecting loop
-
-  if move == 1:
-    if spaceR == 0:
-      detecting = False
-      print("debug detect01")
-  if move == 2:
-    if spaceC == 0:
-      detecting = False
-      print("debug detect01")
-  if move == 3:
-    if spaceC == 2:
-      detecting = False
-      print("debug detect01")
-  if move == 4:
-    if spaceR == 2:
-      detecting = False
-      print("debug detect01")
-"""
       
 
 def moveStep():
@@ -71,6 +42,7 @@ def checkWin():
   if puzzle == [[1,2,3],[4,5,6],[7,8,0]]:
     win = True
 
+
 def getInput():
   global move
   print("How do you want to move the '0'?")
@@ -100,7 +72,7 @@ def ShufflePuzz():
   print(spaceR, ",", spaceC)
   move = 1
 
-  for i in range (0, 100):
+  for i in range (0, 10):
     moveStep()
     move = random.randint(1,4)
     for k in range (0,3):
@@ -108,23 +80,37 @@ def ShufflePuzz():
        if puzzle[k][j] == 0:
          spaceR = k
          spaceC = j
-  
-#main loop:  
-print(move)
-printPuzz()
-print("-------------------------->")
-ShufflePuzz()
-print("-------------------------->")
 
-while win == False:
-  
+def replay():
+  rep = input("Do you want to replay the game? (Y/N)")
+  if rep == "Y":
+    startProcess()
+  elif rep == "N":
+    print("Thank you for playing!")
+
+def startProcess():
+  global win
+  win = False
+  print(move)
   printPuzz()
   print("-------------------------->")
-  print(spaceR, "," ,spaceC)
-  getInput()
-  print("move",move)
+  ShufflePuzz()
   print("-------------------------->")
-  moveStep()
-  checkWin()
   
-print("Congratulation! You won!")
+  while win == False:
+    printPuzz()
+    print("-------------------------->")
+    print(spaceR, "," ,spaceC)
+    getInput()
+    print("move",move)
+    print("-------------------------->")
+    moveStep()
+    checkWin()
+
+  print("Congratulation! You won!\n")
+  replay()
+
+
+#main loop (idk if i should call it a loop, since its only one line now):  
+
+startProcess()
