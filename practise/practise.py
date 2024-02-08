f6dc7df35f5e8af90b5be57ce08174770ebfa48e
SKATATU <edwardhui32406@gmail.com>
2024-02-09 01:50:54 +0800

    finish

---
+++ practise/practise.py
@@ -0,0 +1,18 @@
+"""
+str = ("Hubert is GAY!!!")
+
+print("\n" , str ,"\n" ) 
+
+print((str.lower().islower()))
+
+print("")
+
+for i in range(0,len(str)):
+    print(str[i])
+
+
+str = str.replace(str[13], "X" )
+
+print(str)"""
+
+
