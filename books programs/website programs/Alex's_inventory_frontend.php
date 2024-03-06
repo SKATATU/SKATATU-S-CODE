4dc7be9bf72dc41dedc6311317d96e8fbadc8c5a
SKATATU <edwardhui32406@gmail.com>
2024-03-06 17:46:36 +0800

    added alex's_inventory_frontend and updated LICENSE

--- books programs/website programs/Alex's_inventory_frontend.php
+++ books programs/website programs/Alex's_inventory_frontend.php
@@ -8,6 +8,7 @@
   </head>
   <body>
       <h1>Inventory Data</h1>
+
       <?php
       $file = 'inventory.txt';
       $inventoryData = file($file, FILE_IGNORE_NEW_LINES);//Omit newline at the end of each array element 
@@ -16,16 +17,18 @@
           echo '<tr><th colspan=3>Inventory Data</th> </tr>'; // the title of the table = "Inventory Data"
           echo '<tr><th>Product</th><th>Quantity</th><th>Price</th></tr>'; // cells of the table
 
-          foreach ($inventoryData as $line) {
+          foreach ($inventoryData as $line) // foreach is a loop 
+           {
               $fields = explode(',', $line); 
-              //$product = isset($fields[0]) ? trim($fields[0]) : '';
-              $product = trim($fields[0]) ;
+              //$product = isset($fields[0]) ? trim($fields[0]) : '';  <-- failed line to remove space of the string
+              $product = trim($fields[0]) ; //correct line to remove space from string
               $quantity = trim($fields[1]) ;
               $price = trim($fields[2]) ;
+              
               echo '<tr>';
               echo '<td>'.$product.'</td>';
               echo '<td>'.$quantity.'</td>';
-              echo '<td>'.'$'.$price.'</td>';
+              echo '<td>'.'$'.$price.'</td>'; // the . is the adding operater of strings in php language 
               echo '</tr>';
           }
 
